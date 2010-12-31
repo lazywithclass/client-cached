9c99d536e0fd065f27c7991dd2fb0b228a022844
Alberto Zaccagni <montecristoh@gmail.com>
2010-12-31 01:59:48 +0100

    Version 1.0, seems like it's working :P. I need to test this a bit more though.

--- client-cached.js
+++ client-cached.js
@@ -5,91 +5,111 @@
  * MIT LICENSE
  */
 
-var resultsLocalStorageKey = 'query-results';
-var resultsCountLocalStorageKey = 'count';
-var storedResultsCountLocalStorageKey = 'stored-count';
 
 $('#clearLocalStorage').click(function(){
-    clientCached.initLocalStorage();
+    CLIENTCACHED.initLocalStorage();
 });
 
-//fixme: what about avoiding to call clientCached.PROPERTY every single time? Hm?
-var clientCached = {
-	
-    init : function(parameter){
-        if (!clientCached.hasLocalStorage()) {
-            return;
-        }   
-		//fixme: isn't there a cleaner way?
-		clientCached.resultsDivId = parameter.resultsDivId;
-		clientCached.resultsCountUrl = parameter.resultsCountUrl;
-		clientCached.delay = parameter.delay;
-		
-        $('#'+clientCached.resultsDivId).bind('customAction', function(event, data) {
-            if(! localStorage[resultsLocalStorageKey]){
-                clientCached.initLocalStorage();
-            }
-			localStorage[resultsCountLocalStorageKey] = clientCached.getCount();
-            var uncompressedData = $('#'+clientCached.resultsDivId).html();
-            var compressedData = Iuppiter.compress(uncompressedData);
-            localStorage[resultsLocalStorageKey] = JSON.stringify(compressedData);
-        });
-    },
-	
-    canUseCachedData : function(){
-		if(clientCached.resultsCountUrl==undefined){
-		    if(clientCached.delay==undefined){
-                return false;
-            }else{
-			    //every time the delay expires refresh the cached results
-			}	
-		}else{
-			if(clientCached.delay==undefined){
-				return false;
-            }else{
-	            localStorage[storedResultsCountLocalStorageKey] = clientCached.getCount();
-				if(localStorage[storedResultsCountLocalStorageKey]!=localStorage[resultsCountLocalStorageKey]){
-	                return false;
-	            }else{
-	                return true;
-	            }
-			}
-		}
-		
-        return !clientCached.isLocalStorageEmpty(resultsLocalStorageKey);    
-    },
+var CLIENTCACHED = CLIENTCACHED || {};
 
-    hasLocalStorage : function(){
-        return !!('localStorage' in window && window.localStorage !== null);
-    },
+CLIENTCACHED.storageKeys = {
+	resultsElementId : "",
+	resultsCount : "",
+	startedTime : ""
+};
+
+CLIENTCACHED.hasLocalStorage = function(){
+    return !!('localStorage' in window && window.localStorage !== null);
+};
+
+CLIENTCACHED.init = function(parameter){
+
+	if (!CLIENTCACHED.hasLocalStorage()) {
+        return;
+    }   
     
-    initLocalStorage : function(){
-        localStorage[resultsLocalStorageKey] = JSON.stringify([]);
-		localStorage[resultsCountLocalStorageKey] = JSON.stringify([]);
-		localStorage[storedResultsCountLocalStorageKey] = JSON.stringify([]);
-    },
-    
-    isLocalStorageEmpty : function(which){
-        return localStorage[which]==null || !!(JSON.parse(localStorage[which]).length==0);
-    },
-    
-    populateResults : function(){
-        $('#messages').html("taking data from the localStorage");
-        var compressedData = JSON.parse(localStorage[resultsLocalStorageKey]);
-        $('#'+clientCached.resultsDivId).html(Iuppiter.decompress(compressedData));
-    },
+    //fixme: isn't there a cleaner way?
+    CLIENTCACHED.resultsDivId = parameter.resultsDivId;
+    CLIENTCACHED.resultsCountUrl = parameter.resultsCountUrl;
+    CLIENTCACHED.delay = parameter.delay;
 	
-	getCount : function(){
-		var count = 0;
-		$.ajax({
-            url: clientCached.resultsCountUrl,
-            async: false,
-            type: 'POST',
-            dataType:'json',
-            success: function(response) {
-                 count = response.count;
-            }
-        });
-	    return count;
+    CLIENTCACHED.storageKeys.resultsElementId = parameter.id;
+    CLIENTCACHED.storageKeys.resultsCount = parameter.id+"-count";
+	
+    $('#'+CLIENTCACHED.resultsDivId).bind('customAction', function(event, data) {
+        if(! localStorage[CLIENTCACHED.storageKeys.resultsElementId]){
+            CLIENTCACHED.initLocalStorage();
+        }
+		localStorage[CLIENTCACHED.storageKeys.resultsCount] = CLIENTCACHED.getCount();
+        var uncompressedData = $('#'+CLIENTCACHED.resultsDivId).html();
+        var compressedData = Iuppiter.compress(uncompressedData);
+        localStorage[CLIENTCACHED.storageKeys.resultsElementId] = JSON.stringify(compressedData);
+    });
+};
+
+CLIENTCACHED.canUseCachedData = function(){
+	
+	if(CLIENTCACHED.isLocalStorageEmpty(CLIENTCACHED.storageKeys.startedTime)){
+		updateStartedTime();
 	}
+	
+	if(onlyDelayWasSet()){
+		return !delayExpired();
+	}else if(delayAndCountUrlWereSet()){
+		return !dataChanged();
+	}
+    return !CLIENTCACHED.isLocalStorageEmpty(CLIENTCACHED.storageKeys.resultsElementId);    
+
+    function updateStartedTime(){
+    	localStorage[CLIENTCACHED.storageKeys.startedTime] = new Date().getTime();
+    }
+    
+    function dataChanged(){
+    	return CLIENTCACHED.getCount() != localStorage[CLIENTCACHED.storageKeys.resultsCount];
+    }
+    
+    function delayExpired(){
+    	return localStorage[CLIENTCACHED.storageKeys.startedTime] + CLIENTCACHED.delay > new Date().getTime();
+    }
+    
+    function onlyDelayWasSet(){
+    	if(CLIENTCACHED.resultsCountUrl==undefined){
+    		if(CLIENTCACHED.delay!=undefined){
+    			return true;
+    		}	
+    	}
+    	return false;
+    }
+    
+    function delayAndCountUrlWereSet(){
+    	return CLIENTCACHED.resultsCountUrl!=undefined && CLIENTCACHED.delay!=undefined;
+    }
+};
+
+CLIENTCACHED.initLocalStorage = function(){
+    localStorage[CLIENTCACHED.storageKeys.resultsElementId] = JSON.stringify([]);
+	localStorage[CLIENTCACHED.storageKeys.resultsCount] = JSON.stringify([]);
+};
+
+CLIENTCACHED.isLocalStorageEmpty = function(which){
+    return localStorage[which]==null || !!(JSON.parse(localStorage[which]).length==0);
+};
+
+CLIENTCACHED.populateResults = function(){
+    var compressedData = JSON.parse(localStorage[CLIENTCACHED.storageKeys.resultsElementId]);
+    $('#'+CLIENTCACHED.resultsDivId).html(Iuppiter.decompress(compressedData));
+};
+
+CLIENTCACHED.getCount = function(){
+	var count = 0;
+	$.ajax({
+        url: CLIENTCACHED.resultsCountUrl,
+        async: false,
+        type: 'POST',
+        dataType:'json',
+        success: function(response) {
+             count = response.count;
+        }
+    });
+    return count;
 };
